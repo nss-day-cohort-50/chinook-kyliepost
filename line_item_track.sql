56760e3f82606ff86c7be7017a6a78af9a4a119e
kyliepost <kylie.postlethwaite00@gmail.com>
2021-11-22 09:44:29 -0600

    Provide a query that shows each Invoice line item, with the name of the track that was purchase, and the name of the artist

--- line_item_track.sql
+++ line_item_track.sql
@@ -1,4 +1,8 @@
-SELECT I.InvoiceLineId, T.Name
+SELECT I.InvoiceLineId, T.Name, AR.Artist
 FROM InvoiceLine I
 JOIN Track T
 ON T.TrackId = I.TrackId
+JOIN Album A
+ON A.AlbumId = T.AlbumId
+JOIN Artist AR 
+ON AR.ArtistId = A.ArtistId
